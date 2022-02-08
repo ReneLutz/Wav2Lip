8552bea38a687fe26cc040541c5aa66b7ab66548
Rene Lutz <rene.lutz@sit.fraunhofer.de>
2022-02-08 16:03:02 +0100

    Added setup.py and moved audio to submodule.

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    'torch',
+    'numpy',
+    'opencv-python',
+    'scipy',
+    'numba',
+    'librosa'
+]
+
+setup(
+    name='wav2lip',
+    version='1.0',
+
+    description="Python package for Wav2Lip implementation (https://github.com/Rudrabha/Wav2Lip)",
+
+    url='https://github.com/ReneLutz/Wav2Lip',
+    author='Rene Lutz',
+
+    packages=find_packages(),
+
+    python_requires='>=3',
+    install_requires=requirements,
+
+    classifiers=[
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+    ]
+)
